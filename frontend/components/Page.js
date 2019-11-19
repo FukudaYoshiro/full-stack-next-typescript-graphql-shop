3fce1058589c9e1473deebea1cb9da815378689f
FukudaYoshiro <elsa55queen@gmail.com>
2019-11-19 18:55:16 +0900

    done video no.6

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react'
 import Header from './Header'
 import Meta from './Meta'
-import styled from 'styled-components'
+import styled, { ThemeProvider, injectGlobal } from 'styled-components'
 
-const MyButton = styled.button`
-  background: red;
-  font-size: ${props => props.huge ?'100px' : '50px'};
-  span {
-    font-size: 100px;
-  }
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)'
+}
+
+const StyledPage = styled.div`
+  background: white;
+  color: black;
+`
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  background: ${props => props.theme.red};
+  margin: 0 auto;
+  padding: 2rem;
 `
 
 class Page extends Component {
   render() {
-    return (<div>
+    return (<ThemeProvider theme={theme}><StyledPage>
       <Meta />
       <Header />
-      <MyButton huge>Click Me <span>😀</span></MyButton>
-      <MyButton>Click Me <span>😀</span></MyButton>
-      {this.props.children}
-    </div>)
+      <Inner>{this.props.children}</Inner>
+    </StyledPage></ThemeProvider>)
   }
 }
 
