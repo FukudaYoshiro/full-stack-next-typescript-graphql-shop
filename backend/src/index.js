e3f824585a11ad0badde6450cf187957542378e3
FukudaYoshiro <elsa55queen@gmail.com>
2020-01-23 21:55:33 +0900

    complete video 12

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,18 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' })
+const createServer = require('./createServer')
+const db = require('./db')
+
+const server = createServer()
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  deets => {
+    console.log(`server is now running on port
+    http:/localhost:${deets.port}`)
+  }
+)
