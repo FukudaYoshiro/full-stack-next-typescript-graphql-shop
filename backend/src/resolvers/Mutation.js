545f80676f29c9695ea9f8c4fe65fd79bb594c34
FukudaYoshiro <elsa55queen@gmail.com>
2020-01-24 18:26:56 +0900

    backend: add prettier command

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -5,7 +5,7 @@ const mutations = {
     const newDog = { name: args.name }
     global.dogs.push(newDog)
     return newDog
-  }
-};
+  },
+}
 
-module.exports = mutations;
+module.exports = mutations
--- backend/src/utils.js
+++ backend/src/utils.js
@@ -1,7 +1,7 @@
 function hasPermission(user, permissionsNeeded) {
   const matchedPermissions = user.permissions.filter(permissionTheyHave =>
     permissionsNeeded.includes(permissionTheyHave)
-  );
+  )
   if (!matchedPermissions.length) {
     throw new Error(`You do not have sufficient permissions
 
@@ -10,8 +10,8 @@ function hasPermission(user, permissionsNeeded) {
       You Have:
 
       ${user.permissions}
-      `);
+      `)
   }
 }
 
-exports.hasPermission = hasPermission;
+exports.hasPermission = hasPermission
