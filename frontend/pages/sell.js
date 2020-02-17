9ce3a91357fe8b4a1b7c636c537119d1ba60290c
FukudaYoshiro <elsa55queen@gmail.com>
2020-02-18 02:10:13 +0900

    complete video 33

--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,9 +1,10 @@
 import CreateItem from '../components/CreateItem'
+import PleaseSignIn from '../components/PleaseSignin'
 
 const Sell = props => (
-  <div>
+  <PleaseSignIn>
     <CreateItem />
-  </div>
+  </PleaseSignIn>
 )
 
 export default Sell
