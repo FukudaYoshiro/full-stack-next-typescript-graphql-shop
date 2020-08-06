cd9b48c5e0c62b5d5c575562ac298bae869d4cd3
FukudaYoshiro <elsa55queen@gmail.com>
2020-08-07 08:06:15 +0900

    complete video 38

---
+++ frontend/components/Cart.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import CartStyles from './styles/CartStyles'
+import Supreme from './styles/Supreme'
+import CloseButton from './styles/CloseButton'
+import SickButton from './styles/SickButton'
+
+const Cart = () => {
+  return (
+    <CartStyles open>
+      <header>
+        <CloseButton title="close">&times;</CloseButton>
+        <Supreme>Your Cart</Supreme>
+        <p>You Have __ Items in your cart.</p>
+      </header>
+
+      <footer>
+        <p>$10.10</p>
+        <SickButton>Checkout</SickButton>
+      </footer>
+    </CartStyles>
+  )
+}
+
+export default Cart
--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import styled from 'styled-components'
 import Router from 'next/router'
 import NProgress from 'nprogress'
+import Cart from './Cart'
 
 Router.onRouteChangeStart = () => {
   NProgress.start()
@@ -68,7 +69,7 @@ const Header = () => (
     <div className="sub-bar">
       <p>Search</p>
     </div>
-    <div>Cart</div>
+    <Cart />
   </StyledHeader>
 )
 
