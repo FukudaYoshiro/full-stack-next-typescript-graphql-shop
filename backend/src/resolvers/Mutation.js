a0870b816cbcbfddad3d882715dab101e29adec1
FukudaYoshiro <elsa55queen@gmail.com>
2020-08-06 18:58:43 +0900

    complete video 36

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken')
 const { randomBytes } = require('crypto')
 const { promisify } = require('util')
 const { transport, makeANiceEmail } = require('./../mail')
+const { hasPermission } = require('../utils')
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -157,6 +158,37 @@ const Mutations = {
     })
 
     return updatedUser
+  },
+
+  async updatePermissions(parent, args, ctx, info) {
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in!')
+    }
+
+    const currentUser = await ctx.db.query.user(
+      {
+        where: {
+          id: ctx.request.userId,
+        },
+      },
+      info
+    )
+
+    hasPermission(currentUser, ['ADMIN', 'PERMISSIONUPDATE'])
+
+    return ctx.db.mutation.updateUser(
+      {
+        data: {
+          permissions: {
+            set: args.permissions,
+          },
+        },
+        where: {
+          id: args.userId,
+        },
+      },
+      info
+    )
   },
 }
 
--- frontend/components/styles/Table.js
+++ frontend/components/styles/Table.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 const Table = styled.table`
   border-spacing: 0;
@@ -19,6 +19,10 @@ const Table = styled.table`
       button {
         width: 100%;
       }
+      label {
+        background: red;
+        display: block;
+      }
     }
   }
   tr {
@@ -26,6 +30,6 @@ const Table = styled.table`
       background: ${props => props.theme.offWhite};
     }
   }
-`;
+`
 
-export default Table;
+export default Table
