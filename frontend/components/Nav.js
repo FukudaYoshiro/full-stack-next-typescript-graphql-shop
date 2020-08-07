598fda18beefb92f4451e146d196b517d523fe52
FukudaYoshiro <elsa55queen@gmail.com>
2020-08-07 12:32:24 +0900

    complete video 39

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -2,6 +2,8 @@ import Link from 'next/link'
 import NavStyles from './styles/NavStyles'
 import User from './User'
 import Signout from './Signout'
+import { Mutation } from 'react-apollo'
+import { TOGGLE_CART_MUTATION } from './Cart'
 
 const Nav = () => (
   <User>
@@ -23,6 +25,9 @@ const Nav = () => (
                 <a>Account</a>
               </Link>
               <Signout />
+              <Mutation mutation={TOGGLE_CART_MUTATION}>
+                {toggleCart => <button onClick={toggleCart}>My Cart</button>}
+              </Mutation>
             </>
           )}
           {!me && (
--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,6 +1,7 @@
 import withApollo from 'next-with-apollo'
 import ApolloClient from 'apollo-boost'
 import { endpoint } from '../config'
+import { LOCAL_STATE_QUERY, TOGGLE_CART_MUTATION } from '../components/Cart'
 
 function createClient({ headers }) {
   return new ApolloClient({
@@ -12,6 +13,25 @@ function createClient({ headers }) {
         },
         headers,
       })
+    },
+    clientState: {
+      resolvers: {
+        Mutation: {
+          toggleCart(_, variables, { cache }) {
+            const { cartOpen } = cache.readQuery({
+              query: LOCAL_STATE_QUERY,
+            })
+            const data = {
+              data: { cartOpen: !cartOpen },
+            }
+            cache.writeData(data)
+            return data
+          },
+        },
+      },
+      defaults: {
+        cartOpen: true,
+      },
     },
   })
 }
