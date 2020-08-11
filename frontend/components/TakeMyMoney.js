b1ae70bf0813c859896a70faac16eabc4b7ef671
FukudaYoshiro <elsa55queen@gmail.com>
2020-08-11 20:46:24 +0900

    complete video 50

--- frontend/components/TakeMyMoney.js
+++ frontend/components/TakeMyMoney.js
@@ -8,31 +8,59 @@ import Error from './ErrorMessage'
 import User, { CURRENT_USER_QUERY } from './User'
 import calcTotalPrice from '../lib/calcTotalPrice'
 
+const CREATE_ORDER_MUTATION = gql`
+  mutation createOrder($token: String!) {
+    createOrder(token: $token) {
+      id
+      charge
+      total
+      items {
+        id
+        title
+      }
+    }
+  }
+`
+
 function totalItems(cart) {
   return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)
 }
 
 class TakeMyMoney extends React.Component {
-  onToken = res => {
-    console.log('On Token Called!')
-    console.log(res.id)
+  onToken = (res, createOrder) => {
+    console.log(res)
+    // manually call the mutation once we have the stripe token
+    createOrder({
+      variables: {
+        token: res.id,
+      },
+    }).catch(err => {
+      alert(err.message)
+    })
   }
 
   render() {
     return (
       <User>
         {({ data: { me } }) => (
-          <StripeCheckout
-            amount={calcTotalPrice(me.cart)}
-            name="Sick Fits"
-            description={`Order of ${totalItems(me.cart)} items!`}
-            token={res => this.onToken(res)}
-            stripeKey="pk_test_51HEr8KIdnvxtqnlp2GfIG5y6vPUr1Fj13MxVdPQa6vAGOeaktqAiAroig5TSNBTrIXJg4CSPgjuZNiMhCtr5ZsAS00VFjV10HJ"
-            currency="USD"
-            email={me.email}
+          <Mutation
+            mutation={CREATE_ORDER_MUTATION}
+            refetchQueries={[{ query: CURRENT_USER_QUERY }]}
           >
-            <p>{this.props.children}</p>
-          </StripeCheckout>
+            {createOrder => (
+              <StripeCheckout
+                amount={calcTotalPrice(me.cart)}
+                name="Sick Fits"
+                description={`Order of ${totalItems(me.cart)} items!`}
+                token={res => this.onToken(res, createOrder)}
+                stripeKey="pk_test_51HEr8KIdnvxtqnlp2GfIG5y6vPUr1Fj13MxVdPQa6vAGOeaktqAiAroig5TSNBTrIXJg4CSPgjuZNiMhCtr5ZsAS00VFjV10HJ"
+                currency="USD"
+                email={me.email}
+              >
+                <p>{this.props.children}</p>
+              </StripeCheckout>
+            )}
+          </Mutation>
         )}
       </User>
     )
