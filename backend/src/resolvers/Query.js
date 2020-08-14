9fb234c349e53c4624b0aad766a15807f093fdde
FukudaYoshiro <elsa55queen@gmail.com>
2020-08-15 03:15:36 +0900

    complete video 52

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -27,6 +27,28 @@ const Query = {
     // 2.if they do, query add the users!
     return ctx.db.query.users({}, info)
   },
+
+  async order(parent, args, ctx, info) {
+    // 1. Make sure they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You arent logged in!')
+    }
+    // 2. Query the current order
+    const order = await ctx.db.query.order({
+      where: { id: args.id },
+      info,
+    })
+    // 3. Check if the have the permission to see this order
+    const ownsOrder = order.user.id === ctx.request.userId
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.include(
+      'ADMIN'
+    )
+    if (!ownsOrder || !hasPermission) {
+      throw new Error('You cant see this buddd')
+    }
+    // 4. Return the order
+    return order
+  },
 }
 
 module.exports = Query
--- frontend/components/TakeMyMoney.js
+++ frontend/components/TakeMyMoney.js
@@ -28,6 +28,7 @@ function totalItems(cart) {
 
 class TakeMyMoney extends React.Component {
   onToken = async (res, createOrder) => {
+    NProgress.start()
     // manually call the mutation once we have the stripe token
     const order = await createOrder({
       variables: {
@@ -36,7 +37,10 @@ class TakeMyMoney extends React.Component {
     }).catch(err => {
       alert(err.message)
     })
-    console.log(order)
+    Router.push({
+      pathname: '/order',
+      query: { id: order.data.createOrder.id },
+    })
   }
 
   render() {
